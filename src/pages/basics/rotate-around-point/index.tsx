2c20a003dff759b3badd3617e496bc0704f308a9
Kryštof Matějka <kx.matejka@gmail.com>
2019-08-11 20:22:22 +0200

    feat: dynamic change speed of rotation

--- src/pages/basics/rotate-around-point/index.tsx
+++ src/pages/basics/rotate-around-point/index.tsx
@@ -1,15 +1,18 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import Konva from 'konva'
 import { Layer, Circle } from 'react-konva'
 import { BaseCanvas, BaseCanvasSizeContext } from '../../../components/canvas'
 
+const updateSpeed = (setSpeed) => (event) => parseFloat(setSpeed(event.target.value))
+
 const RotateAroundPoint = () => {
+  const [speed, setSpeed] = useState(1.5)
   const rotatingCircle = useRef(null)
   const [canvasWidth, canvasHeight] = useContext(BaseCanvasSizeContext)
 
   useEffect(() => {
     const animation = new Konva.Animation(() => {
-      const angle = 1.5 * Math.PI / 180
+      const angle = speed * Math.PI / 180
       const sin = Math.sin(angle)
       const cos = Math.cos(angle)
 
@@ -23,23 +26,29 @@ const RotateAroundPoint = () => {
     animation.start()
 
     return () => animation.stop()
-  }, [])
+  }, [speed])
 
   return (
-    <BaseCanvas>
-      <Layer>
-        <Circle radius={25}
-                x={canvasWidth / 2} y={canvasHeight / 2}
-                fill={'#ffda22'}
-        />
-        <Circle radius={10}
-                x={0} y={0}
-                offsetX={-150} offsetY={-150}
-                fill={'#3487ff'}
-                ref={rotatingCircle}
-        />
-      </Layer>
-    </BaseCanvas>
+    <>
+      <label>
+        <p>Speed: {speed * 10} %</p>
+        <input type={'range'} min={0} max={10} step={0.1} value={speed} onChange={updateSpeed(setSpeed)} />
+      </label>
+      <BaseCanvas>
+        <Layer>
+          <Circle radius={25}
+                  x={canvasWidth / 2} y={canvasHeight / 2}
+                  fill={'#ffda22'}
+          />
+          <Circle radius={10}
+                  x={0} y={0}
+                  offsetX={-150} offsetY={-150}
+                  fill={'#3487ff'}
+                  ref={rotatingCircle}
+          />
+        </Layer>
+      </BaseCanvas>
+    </>
   )
 }
 
